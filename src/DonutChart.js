e12087b479a6d0853e3e01ea7eb5b13471a09157
Chenrui666 <31868437+Chenrui666@users.noreply.github.com>
2018-04-06 09:28:59 -0400

    Data path from dist

--- src/DonutChart.js
+++ src/DonutChart.js
@@ -31,7 +31,20 @@ function DonutChart(_){
 		.append('g')
 		    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 		 
-	
+	const g = svg.selectAll(".arc")
+		      .data(pie(data))
+		    .enter().append("g")
+		      .attr("class", "arc");
+
+		  g.append("path")
+		      .attr("d", arc)
+		      .style("fill", function(d) { return color(d.SOC_TYPE); });
+
+		  g.append("text")
+		      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		      .attr("dy", ".35em")
+		      .text(function(d) { return d.SOC_TYPE; });
+
 
 
 	}
--- src/index.js
+++ src/index.js
@@ -8,20 +8,7 @@ import DonutChart from './DonutChart';
 
 Promise.all([d3.csv('./H-1B_Disclosure_Data_FY17.csv')])
 	.then(([error,data]) => {
-		const g = svg.selectAll(".arc")
-		      .data(pie(data))
-		    .enter().append("g")
-		      .attr("class", "arc");
-
-		  g.append("path")
-		      .attr("d", arc)
-		      .style("fill", function(d) { return color(d.SOC_TYPE); });
-
-		  g.append("text")
-		      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-		      .attr("dy", ".35em")
-		      .text(function(d) { return d.SOC_TYPE; });
-
+		
 
 		  d3.select('#occupation-distribution')
 		  	.datum(data)
